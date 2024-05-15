ad5ffbd0c90c4eac04a6228b954fa5bc9e02133d
denis-orbs <denis@orbs.com>
2024-05-15 16:17:51 +0300

    a

--- src/lib/hooks/useSwapDetails.ts
+++ src/lib/hooks/useSwapDetails.ts
@@ -38,106 +38,106 @@ export function useUsdAmounts() {
 }
 
 export function usePriceImpact() {
-  const {inTokenUsdAmount, outTokenUsdAmount} = useUsdAmounts();
+  const { inTokenUsdAmount, outTokenUsdAmount } = useUsdAmounts();
 
   return useMemo(() => {
-    return BN(outTokenUsdAmount || "0")
-      .div(inTokenUsdAmount || "0")
+    if (
+      BN(outTokenUsdAmount || "0").isZero() ||
+      BN(inTokenUsdAmount || "0").isZero()
+    )
+      return;
+    return BN(outTokenUsdAmount)
+      .div(inTokenUsdAmount)
       .minus(1)
       .times(100)
       .toString();
   }, [inTokenUsdAmount, outTokenUsdAmount]);
 }
 
-
 export function useGasCostUsd() {
-    const outTokenUsd = useSwapState(useShallow(s => s.outTokenUsd));
-    const gasCostOutputToken = useQuote().data?.gasCostOutputToken;
-    const toToken = useSwapState(useShallow(s => s.toToken))
-    return useMemo(() => {
-      if (!gasCostOutputToken || !outTokenUsd) return;
-      return amountUi(toToken?.decimals, BN(gasCostOutputToken).multipliedBy(outTokenUsd));
-    }, [gasCostOutputToken, outTokenUsd, toToken]);
-  }
-  
-  
-  
-
-  export const useEstimateGasPrice = () => {
-    const { web3, chainId } = useMainContext();
-    return useQuery({
-      queryKey: [QUERY_KEYS.GAS_PRICE, chainId],
-      queryFn: async () => {
-        const result = await estimateGasPrice(web3!, chainId!);
-          const priorityFeePerGas = result?.fast.tip || 0;
-          const maxFeePerGas = BN.max(
-            result?.fast.max || 0,
-            priorityFeePerGas
-          );
-  
-          return {
-            result,
-            priorityFeePerGas,
-            maxFeePerGas,
-          };
-      },
-      refetchInterval: 15_000,
-      enabled: !!web3 && !!chainId,
-    });
-  };
-  
-
-  export function useSlippage() {
-    const contextSlippage = useMainContext().slippage;
-    const storeSlippage = useSwapState(useShallow((s) => s.slippage));    
-    const slippage = storeSlippage || contextSlippage
-      
-    return useMemo(() => {
-      if (!slippage) return 0;
-      return BN(slippage).isNaN() ? 0 : slippage;
-    }, [slippage]);
-  
-  }
-  
-
-  export const useSwapConfirmation = () => {
-    const store = useSwapState(
-      useShallow((s) => ({
-        fromToken: s.fromToken,
-        toToken: s.toToken,
-        txHash: s.txHash,
-        swapStatus: s.swapStatus,
-        swapError: s.swapError,
-        showConfirmation: s.showConfirmation,
-        fromAmount: s.fromAmount,
-        disableLh: s.disableLh,
-        onCloseSwap: s.onCloseSwap,
-      }))
+  const outTokenUsd = useSwapState(useShallow((s) => s.outTokenUsd));
+  const gasCostOutputToken = useQuote().data?.gasCostOutputToken;
+  const toToken = useSwapState(useShallow((s) => s.toToken));
+  return useMemo(() => {
+    if (!gasCostOutputToken || !outTokenUsd) return;
+    return amountUi(
+      toToken?.decimals,
+      BN(gasCostOutputToken).multipliedBy(outTokenUsd)
     );
+  }, [gasCostOutputToken, outTokenUsd, toToken]);
+}
 
-    const title = useMemo(() => {
-      if (store.swapStatus === "failed") {
-        return;
-      }
-      if (store.swapStatus === "success") {
-        return "Swap Successfull";
-      }
-      return "Review Swap";
-    }, [store.swapStatus]);
-    const fromAmountUI = useAmountUI(store.fromToken?.decimals, store.fromAmount);
-    const outAmount = useAmountUI(store.toToken?.decimals, useQuote().data?.outAmount);
+export const useEstimateGasPrice = () => {
+  const { web3, chainId } = useMainContext();
+  return useQuery({
+    queryKey: [QUERY_KEYS.GAS_PRICE, chainId],
+    queryFn: async () => {
+      const result = await estimateGasPrice(web3!, chainId!);
+      const priorityFeePerGas = result?.fast.tip || 0;
+      const maxFeePerGas = BN.max(result?.fast.max || 0, priorityFeePerGas);
 
-    return {
-      fromToken: store.fromToken,
-      toToken: store.toToken,
-      fromAmount: fromAmountUI,
-      txHash: store.txHash,
-      swapStatus: store.swapStatus,
-      swapError: store.swapError,
-      isOpen: !!store.showConfirmation,
-      onClose: store.onCloseSwap,
-      title,
-      outAmount
-    };
+      return {
+        result,
+        priorityFeePerGas,
+        maxFeePerGas,
+      };
+    },
+    refetchInterval: 15_000,
+    enabled: !!web3 && !!chainId,
+  });
+};
+
+export function useSlippage() {
+  const contextSlippage = useMainContext().slippage;
+  const storeSlippage = useSwapState(useShallow((s) => s.slippage));
+  const slippage = storeSlippage || contextSlippage;
+
+  return useMemo(() => {
+    if (!slippage) return 0;
+    return BN(slippage).isNaN() ? 0 : slippage;
+  }, [slippage]);
+}
+
+export const useSwapConfirmation = () => {
+  const store = useSwapState(
+    useShallow((s) => ({
+      fromToken: s.fromToken,
+      toToken: s.toToken,
+      txHash: s.txHash,
+      swapStatus: s.swapStatus,
+      swapError: s.swapError,
+      showConfirmation: s.showConfirmation,
+      fromAmount: s.fromAmount,
+      disableLh: s.disableLh,
+      onCloseSwap: s.onCloseSwap,
+    }))
+  );
+
+  const title = useMemo(() => {
+    if (store.swapStatus === "failed") {
+      return;
+    }
+    if (store.swapStatus === "success") {
+      return "Swap Successfull";
+    }
+    return "Review Swap";
+  }, [store.swapStatus]);
+  const fromAmountUI = useAmountUI(store.fromToken?.decimals, store.fromAmount);
+  const outAmount = useAmountUI(
+    store.toToken?.decimals,
+    useQuote().data?.outAmount
+  );
+
+  return {
+    fromToken: store.fromToken,
+    toToken: store.toToken,
+    fromAmount: fromAmountUI,
+    txHash: store.txHash,
+    swapStatus: store.swapStatus,
+    swapError: store.swapError,
+    isOpen: !!store.showConfirmation,
+    onClose: store.onCloseSwap,
+    title,
+    outAmount,
   };
-  
+};
